afb71ea4dd19d2449a6e01403e8d45e97ba69dc6
nur <nuraalamp@gmail.com>
2022-01-16 21:37:57 +0600

    csv visualization added

--- Task/app.py
+++ Task/app.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template,request,redirect, url_for, abort, \
-    send_from_directory
-from werkzeug.utils import secure_filename
 import os
+from werkzeug.utils import secure_filename
+import json
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory, Response
+
 from test import ReadData
 from totalwork import TotalWork
 from LeaveHolidayAbsent import LeaveHolidayAbsent
@@ -9,14 +12,14 @@ from LeaveHolidayAbsent import LeaveHolidayAbsent
 
 # import imghdr
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.csv','.CSV']
+app.config['UPLOAD_EXTENSIONS'] = ['.csv', '.CSV']
 app.config['UPLOAD_PATH'] = './'
 app.secret_key = 'BAD_SECRET_KEY'
 
 
-session={}
+session = {}
 
 
 @app.errorhandler(413)
@@ -28,7 +31,8 @@ def too_large(e):
 def home():
     return render_template("home.html")
 
-@app.route('/',methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def upload_files():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
@@ -37,10 +41,22 @@ def upload_files():
         print(file_ext)
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             return "Not a CSV", 400
+        head = {'Date',
+                'Employee Id',
+                'First half end',
+                'First half start',
+                'Second half end',
+                'Second half start'}
+        
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        session['FILE_NAME']=filename
-    return '', 204
-
+        session['FILE_NAME'] = filename
+        df = pd.read_csv(session['FILE_NAME'])
+        if set(df.columns) != head:
+            return {"error":"error"},409
+        table = df[:5].to_html(classes='mystyle',index=False).replace(
+            'border="1"', 'border="0"')
+        data = {"filename": filename, "table": table}
+    return Response(json.dumps(data))
 
 
 @app.route("/data")
@@ -50,15 +66,13 @@ def data():
 
         totalwork = TotalWork(data.read_data)
         total_work_in_month_values, total_work_in_month_bar \
-        = [x[0] for x in totalwork.total_work_hour[0]],\
-        [x[1] for x in totalwork.total_work_hour[0]]
+            = [x[0] for x in totalwork.total_work_hour[0]],\
+            [x[1] for x in totalwork.total_work_hour[0]]
 
         total_work_sum = totalwork.total_work_hour[1]
 
-
-        bar_values,bar_labels = [x[0] for x in totalwork.overtime],\
-        [x[1] for x in totalwork.overtime]
-        
+        bar_values, bar_labels = [x[0] for x in totalwork.overtime],\
+            [x[1] for x in totalwork.overtime]
 
         LHA = LeaveHolidayAbsent(data.read_data)
         absent = LHA.absent
@@ -67,14 +81,14 @@ def data():
         present = LHA.present
 
         return render_template('bar_chart.html', title='Bitcoin Monthly Price \
-        in USD', max=17000, labels=bar_labels, values=bar_values,\
-        absent = absent,\
-        holiday = holiday,\
-        leave = leave,\
-        present = present,\
-        total_work_in_month_bar = total_work_in_month_bar,\
-        total_work_in_month_values = total_work_in_month_values,\
-        total_work_sum =total_work_sum)
+        in USD', max=17000, labels=bar_labels, values=bar_values,
+                               absent=absent,
+                               holiday=holiday,
+                               leave=leave,
+                               present=present,
+                               total_work_in_month_bar=total_work_in_month_bar,
+                               total_work_in_month_values=total_work_in_month_values,
+                               total_work_sum=total_work_sum)
     return """
         <!DOCTYPE html>
             <html>
@@ -87,6 +101,7 @@ def data():
             </html>
         """
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     # app.run()
     app.run(debug=True)
